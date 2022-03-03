5a3df77d466ffadae4578a0e5f53412fd79c700d
selcukaltinay <selcukaltinay@outlook.com>
2022-03-04 02:48:01 +0300

    61th day completed

---
+++ lecture_50_namespace.php
@@ -0,0 +1,19 @@
+<?php
+include "Html.php";
+
+$table = new Html\Table();
+$table->title = "My table";
+$table->numRows = 5;
+
+$row = new Html\Row();
+$row->numCells = 3;
+?>
+
+<html>
+<body>
+
+<?php $table->message(); ?>
+<?php $row->message(); ?>
+
+</body>
+</html>
